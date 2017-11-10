c494aa42328eabc818244cf27d33c2293581213b
gnasherx <pawarg124@gmail.com>
2017-11-10 14:15:12 +0530

    basic user

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 /* eslint-disable no-console */
-import express from 'express';
+import express, { Router } from 'express';
 
 import constants from './config/constants';
 import './config/db';
 import middlewares from './config/middlewares';
+import Routes from './modules';
 
 const app = express();
+
 middlewares(app);
+
+app.use('/api/v1', Routes);
 
 app.listen(constants.PORT, (err) => {
   if (err) {
